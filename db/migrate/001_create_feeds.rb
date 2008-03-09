bc69357f7c7b8cb4b366839a51d4e802ff27ba41
Cameron Booth <cameron@hysteria.local>
2008-03-09 16:13:24 -0500

    Files from generating Feed model

---
+++ db/migrate/001_create_feeds.rb
@@ -0,0 +1,15 @@
+class CreateFeeds < ActiveRecord::Migration
+  def self.up
+    create_table :feeds do |t|
+      t.string :name
+      t.string :url
+      t.datetime :last_accessed
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :feeds
+  end
+end
